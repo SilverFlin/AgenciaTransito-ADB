df9d96c791b319f50b9593663619079ef293afe6
DESKTOP-4RU6P74\march <misael.marchena233418@potros.itson.edu.mx>
2023-03-28 11:06:33 -0700

    feat: cambié el menu princpal

--- 03_Implementation/agencia_transito/src/main/java/org/itson/main/Main.java
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/main/Main.java
@@ -11,6 +11,6 @@ package org.itson.main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Toledo!");
     }
 }
