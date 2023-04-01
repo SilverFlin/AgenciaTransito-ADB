bbd8306a6ee2c0934d7e748dfc53ea99c0ab8b47
SilverFlin <toledorusso@outlook.com>
2023-03-31 20:17:11 -0700

    feat: pagosDAO

---
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/daos/PagosDAO.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.daos;
+
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import org.itson.dominio.Pago;
+import org.itson.excepciones.PersistenciaException;
+
+/**
+ *
+ * @author Toled
+ */
+public class PagosDAO implements DAO<Pago> {
+
+    @PersistenceContext(unitName = "agencia_transito")
+    private EntityManager entityManager;
+
+    public PagosDAO() {
+        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("agencia_transito");
+        this.entityManager = managerFactory.createEntityManager();
+    }
+
+    @Override
+    public Optional<Pago> get(long id) {
+        return Optional.ofNullable(entityManager.find(Pago.class, id));
+    }
+
+    @Override
+    public List<Pago> getAll() {
+        String codigoJPQL = "SELECT p FROM Pago p ";
+        TypedQuery<Pago> query = entityManager.createQuery(codigoJPQL, Pago.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public Pago save(Pago pago) throws PersistenciaException {
+        try {
+            entityManager.getTransaction().begin();
+
+            entityManager.persist(pago);
+
+            entityManager.getTransaction().commit();
+            return pago;
+        } catch (Exception e) {
+            throw new PersistenciaException("Error al guardar pago");
+        }
+    }
+
+    @Override
+    public Pago update(Pago t, String[] params) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Pago delete(Pago t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
