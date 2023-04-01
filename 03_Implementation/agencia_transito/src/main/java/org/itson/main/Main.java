683d8a9a850aa546d91a230683fdbd7babe083c1
SilverFlin <toledorusso@outlook.com>
2023-04-01 09:34:10 -0700

    style: lint errors

--- 03_Implementation/agencia_transito/src/main/java/org/itson/main/Main.java
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/main/Main.java
@@ -1,37 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 package org.itson.main;
-
-import java.util.List;
-import java.util.Random;
-import org.itson.temptest.*;//Borrar
 
 /**
  *
  * @author Toled
  */
-public class Main {
+final class Main {
 
-    public static void main(String[] args) {
-        TestTramitesDAO.probarTramitesDAO();
+    private Main() {
     }
 
-    // Pseudo-pruebas 
-    // TODO pasar a unit test
-    public static <T> void imprimirLista(List<T> lista) {
-        for (Object obj : lista) {
-            System.out.println(obj);
-        }
-    }
-
-    public static String getRandomNumber(int digCount) {
-        Random rnd = new Random();
-        StringBuilder sb = new StringBuilder(digCount);
-        for (int i = 0; i < digCount; i++) {
-            sb.append((char) ('0' + rnd.nextInt(10)));
-        }
-        return sb.toString();
+    /**
+     * Inicia el sistema.
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        // TODO
     }
 
 }
---
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/main/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Este modulo se encarga de la inicialización del proyecto
+ * así como herramientas de apoyo durante el desarrollo del sistema.
+ *
+ * @author Toled
+ */
+package org.itson.main;
