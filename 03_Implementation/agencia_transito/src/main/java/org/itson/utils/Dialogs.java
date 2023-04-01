c7a29add0c1e61b906f4de42a02263734e3ab061
SilverFlin <toledorusso@outlook.com>
2023-03-31 21:25:49 -0700

    feat: util dialogs

---
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/utils/Dialogs.java
@@ -0,0 +1,22 @@
+package org.itson.utils;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Toled
+ */
+public class Dialogs {
+     public static void mostrarMensajeError(Component componente, String msg) {
+        JOptionPane.showMessageDialog(componente, msg, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void mostrarMensajeExito(Component componente, String msg) {
+        JOptionPane.showMessageDialog(componente, msg, "Exito", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    public static int mostrarMensajeYesNoOption(Component componente, String msg, String titulo) {
+        return JOptionPane.showConfirmDialog(componente, msg, titulo, JOptionPane.YES_NO_OPTION);
+    }
+}
