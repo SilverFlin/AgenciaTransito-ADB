c0c53ba59901442896e48a53e3428a9ae4de1c98
SilverFlin <toledorusso@outlook.com>
2023-03-31 17:08:31 -0700

    feat: personas dao

--- 03_Implementation/agencia_transito/src/main/java/org/itson/daos/PersonasDAO.java
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/daos/PersonasDAO.java
@@ -4,37 +4,67 @@
  */
 package org.itson.daos;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import org.itson.dominio.Persona;
+import org.itson.excepciones.PersistenciaException;
 
 /**
  *
  * @author Toled
  */
-public class PersonasDAO {
+public class PersonasDAO implements DAO<Persona> {
     
-    public void insertar(Persona persona){
+    @PersistenceContext(unitName = "agencia_transito")
+    private EntityManager entityManager;
+    
+    public PersonasDAO() {
         EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("agencia_transito");
-        EntityManager entityManager = managerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        
-        entityManager.persist(persona);
-
-        entityManager.getTransaction().commit();
+        this.entityManager = managerFactory.createEntityManager();
     }
     
-    public static List<Persona> buscar() {
-        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("agencia_transito");
-        EntityManager entityManager = managerFactory.createEntityManager();
-
+    @Override
+    public Optional<Persona> get(long id) {
+        return Optional.ofNullable(entityManager.find(Persona.class, id));
+    }
+    
+    @Override
+    public List<Persona> getAll() {
+        
         String codigoJPQL = "SELECT p FROM Persona p ";
         TypedQuery<Persona> query = entityManager.createQuery(codigoJPQL, Persona.class);
         
         return query.getResultList();
+    }
+    
+    @Override
+    public void save(Persona persona) throws PersistenciaException {
+        try {
+            entityManager.getTransaction().begin();
+            
+            entityManager.persist(persona);
+            
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            throw new PersistenciaException("Error al guardar persona");
+        }
+        
+    }
+    
+    @Override
+    public void update(Persona t, String[] params) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public void delete(Persona t) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
--- 03_Implementation/agencia_transito/src/main/java/org/itson/main/Main.java
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/main/Main.java
@@ -3,9 +3,18 @@
  */
 package org.itson.main;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.itson.daos.DAO;
 import org.itson.daos.PersonasDAO;
 import org.itson.dominio.Persona;
+import org.itson.excepciones.PersistenciaException;
 
 /**
  *
@@ -14,18 +23,71 @@ import org.itson.dominio.Persona;
 public class Main {
 
     public static void main(String[] args) {
-        consultaConDaoJPQL();
+        probarPersoansDAO();
     }
 
-    public static void consultaConDaoJPQL() {
-        List<Persona> personas = PersonasDAO.buscar();
-        imprimirLista(personas);
+    // Pseudo-pruebas 
+    // TODO pasar a unit test
+    public static void probarPersoansDAO() {
+        System.out.println("Agregar Persona falsa:");
+        agregaPersonaFalsa();
+        System.out.println("\nConsultar Personas");
+        consultaPersonas();
+        System.out.println("\nConsultar persona id=1:");
+        consultaPrimerPersona();
+    }
 
+    public static void consultaPersonas() {
+        DAO personasDAO = new PersonasDAO();
+        List<Persona> personas = personasDAO.getAll();
+        imprimirLista(personas);
+    }
+
+    public static void consultaPrimerPersona() {
+        DAO personasDAO = new PersonasDAO();
+        Optional persona = personasDAO.get(1);
+        System.out.println(persona);
+    }
+
+    public static void agregaPersonaFalsa() {
+        Persona persona = crearPersonaFalsa();
+        DAO personasDAO = new PersonasDAO();
+        try {
+            personasDAO.save(persona);
+        } catch (PersistenciaException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     private static <T> void imprimirLista(List<T> lista) {
         for (Object obj : lista) {
             System.out.println(obj);
         }
+    }
+
+    private static Persona crearPersonaFalsa() {
+        Persona persona = new Persona();
+        persona.setNombres("Jedal");
+        persona.setApellidoPaterno("Lestibournes");
+        persona.setApellidoMaterno("The TinEye");
+
+        persona.setFechaNacimiento(new GregorianCalendar(1007, 12, 6));
+        persona.setRFC(getRandomNumber(13));
+        persona.setTelefono("123123123");
+
+        return persona;
+    }
+
+    public static String getRandomNumber(int digCount) {
+        Random rnd = new Random();
+        StringBuilder sb = new StringBuilder(digCount);
+        for (int i = 0; i < digCount; i++) {
+            sb.append((char) ('0' + rnd.nextInt(10)));
+        }
+        return sb.toString();
+    }
+
+    public static String getRandomRFC() {
+        return getRandomNumber(13);
     }
 }
