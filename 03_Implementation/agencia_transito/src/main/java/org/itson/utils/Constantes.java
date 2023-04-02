f4d8aac287ddd0cd6c0756954d3de41d26f9fe1c
SilverFlin <toledorusso@outlook.com>
2023-04-02 13:08:46 -0700

    feat: constantes globales

---
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/utils/Constantes.java
@@ -0,0 +1,16 @@
+package org.itson.utils;
+
+/**
+ *
+ * @author Toled
+ */
+public class Constantes {
+
+    private Constantes() {
+        throw new IllegalStateException("Utility class");
+    }
+    /**
+     * Nombre de la unidad de persistancia utilizada en el sistema.
+     */
+    public static final String PERSISTENCE_UNIT = "agencia_transito";
+}
