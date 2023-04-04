4e22aa6c1c86af54e4b9fa999101c022018c39ab
SilverFlin <toledorusso@outlook.com>
2023-04-04 13:09:57 -0700

    feat: test validaciones

--- 03_Implementation/agencia_transito/src/test/java/org/itson/utils/FormateadorTelefonoTest.java
+++ 03_Implementation/agencia_transito/src/test/java/org/itson/utils/FormateadorTelefonoTest.java
@@ -1,11 +1,7 @@
 package org.itson.utils;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /**
