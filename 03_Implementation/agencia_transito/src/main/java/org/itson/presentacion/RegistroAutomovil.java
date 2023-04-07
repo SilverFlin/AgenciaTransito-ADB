9514cc71908eb91b97fc4126251656d8ea18e8d0
DESKTOP-4RU6P74\march <misael.marchena233418@potros.itson.edu.mx>
2023-04-06 22:57:27 -0700

    cambios en el formulario RegistroAutomovil

--- 03_Implementation/agencia_transito/src/main/java/org/itson/presentacion/RegistroAutomovil.java
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/presentacion/RegistroAutomovil.java
@@ -27,33 +27,33 @@ public class RegistroAutomovil extends javax.swing.JFrame {
         lblBienvenido = new javax.swing.JLabel();
         btnRegresar = new javax.swing.JButton();
         btnRegistrarAutomovil = new javax.swing.JButton();
-        btnRegistrarAutomovil1 = new javax.swing.JButton();
+        btnCargarDatos = new javax.swing.JButton();
         lblNombres = new javax.swing.JLabel();
-        campoTextoNombres = new javax.swing.JTextField();
+        txtNombres = new javax.swing.JTextField();
         lblApellidoPaterno = new javax.swing.JLabel();
-        campoTextoApellidoPaterno = new javax.swing.JTextField();
+        txtApellidoPaterno = new javax.swing.JTextField();
         lblApellidoMaterno = new javax.swing.JLabel();
-        campoTextoApellidoMaterno = new javax.swing.JTextField();
-        lblEdad = new javax.swing.JLabel();
-        campoTextoEdad = new javax.swing.JTextField();
+        txtApellidoMaterno = new javax.swing.JTextField();
+        lblRFC = new javax.swing.JLabel();
+        txtRFC = new javax.swing.JTextField();
         jSeparator2 = new javax.swing.JSeparator();
         jSeparator3 = new javax.swing.JSeparator();
         jSeparator4 = new javax.swing.JSeparator();
         jSeparator1 = new javax.swing.JSeparator();
-        lblNombres1 = new javax.swing.JLabel();
-        campoTextoNombres1 = new javax.swing.JTextField();
+        lblMarca = new javax.swing.JLabel();
+        txtMarca = new javax.swing.JTextField();
         jSeparator5 = new javax.swing.JSeparator();
-        lblApellidoPaterno1 = new javax.swing.JLabel();
-        campoTextoApellidoPaterno1 = new javax.swing.JTextField();
+        lblLinea = new javax.swing.JLabel();
+        txtLinea = new javax.swing.JTextField();
         jSeparator6 = new javax.swing.JSeparator();
-        lblApellidoMaterno1 = new javax.swing.JLabel();
-        campoTextoApellidoMaterno1 = new javax.swing.JTextField();
+        lblModelo = new javax.swing.JLabel();
+        txtModelo = new javax.swing.JTextField();
         jSeparator7 = new javax.swing.JSeparator();
-        lblNombres2 = new javax.swing.JLabel();
-        campoTextoNombres2 = new javax.swing.JTextField();
+        lblColor = new javax.swing.JLabel();
+        txtColor = new javax.swing.JTextField();
         jSeparator8 = new javax.swing.JSeparator();
-        lblApellidoPaterno2 = new javax.swing.JLabel();
-        campoTextoApellidoPaterno2 = new javax.swing.JTextField();
+        lblSerie = new javax.swing.JLabel();
+        txtSerie = new javax.swing.JTextField();
         jSeparator9 = new javax.swing.JSeparator();
         jSeparator11 = new javax.swing.JSeparator();
 
@@ -107,108 +107,117 @@ public class RegistroAutomovil extends javax.swing.JFrame {
         });
         Background.add(btnRegistrarAutomovil, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 300, 100, 30));
 
-        btnRegistrarAutomovil1.setBackground(new java.awt.Color(121, 90, 59));
-        btnRegistrarAutomovil1.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
-        btnRegistrarAutomovil1.setForeground(new java.awt.Color(255, 255, 255));
-        btnRegistrarAutomovil1.setText("Cargar datos");
-        btnRegistrarAutomovil1.setBorder(null);
-        btnRegistrarAutomovil1.setBorderPainted(false);
-        btnRegistrarAutomovil1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        btnRegistrarAutomovil1.addActionListener(new java.awt.event.ActionListener() {
+        btnCargarDatos.setBackground(new java.awt.Color(121, 90, 59));
+        btnCargarDatos.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        btnCargarDatos.setForeground(new java.awt.Color(255, 255, 255));
+        btnCargarDatos.setText("Cargar datos");
+        btnCargarDatos.setBorder(null);
+        btnCargarDatos.setBorderPainted(false);
+        btnCargarDatos.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btnCargarDatos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnRegistrarAutomovil1ActionPerformed(evt);
+                btnCargarDatosActionPerformed(evt);
             }
         });
-        Background.add(btnRegistrarAutomovil1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 110, 100, 30));
+        Background.add(btnCargarDatos, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 110, 100, 30));
 
-        lblNombres.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 14)); // NOI18N
+        lblNombres.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        lblNombres.setForeground(new java.awt.Color(255, 255, 255));
         lblNombres.setText("Nombres");
         Background.add(lblNombres, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 160, 180, 20));
 
-        campoTextoNombres.setForeground(new java.awt.Color(51, 51, 51));
-        campoTextoNombres.setToolTipText("");
-        campoTextoNombres.setBorder(null);
-        Background.add(campoTextoNombres, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 180, 170, 20));
+        txtNombres.setForeground(new java.awt.Color(51, 51, 51));
+        txtNombres.setToolTipText("");
+        txtNombres.setBorder(null);
+        Background.add(txtNombres, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 180, 170, 20));
 
-        lblApellidoPaterno.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 14)); // NOI18N
+        lblApellidoPaterno.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        lblApellidoPaterno.setForeground(new java.awt.Color(255, 255, 255));
         lblApellidoPaterno.setText("Apellido paterno");
         Background.add(lblApellidoPaterno, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 160, 180, 20));
 
-        campoTextoApellidoPaterno.setForeground(new java.awt.Color(51, 51, 51));
-        campoTextoApellidoPaterno.setToolTipText("");
-        campoTextoApellidoPaterno.setBorder(null);
-        Background.add(campoTextoApellidoPaterno, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 180, 160, 20));
+        txtApellidoPaterno.setForeground(new java.awt.Color(51, 51, 51));
+        txtApellidoPaterno.setToolTipText("");
+        txtApellidoPaterno.setBorder(null);
+        Background.add(txtApellidoPaterno, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 180, 160, 20));
 
-        lblApellidoMaterno.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 14)); // NOI18N
+        lblApellidoMaterno.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        lblApellidoMaterno.setForeground(new java.awt.Color(255, 255, 255));
         lblApellidoMaterno.setText("Apellido materno");
         Background.add(lblApellidoMaterno, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 160, 130, 20));
 
-        campoTextoApellidoMaterno.setForeground(new java.awt.Color(51, 51, 51));
-        campoTextoApellidoMaterno.setToolTipText("");
-        campoTextoApellidoMaterno.setBorder(null);
-        Background.add(campoTextoApellidoMaterno, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 180, 160, 20));
+        txtApellidoMaterno.setForeground(new java.awt.Color(51, 51, 51));
+        txtApellidoMaterno.setToolTipText("");
+        txtApellidoMaterno.setBorder(null);
+        Background.add(txtApellidoMaterno, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 180, 160, 20));
 
-        lblEdad.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 14)); // NOI18N
-        lblEdad.setText("RFC");
-        Background.add(lblEdad, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, -1, -1));
+        lblRFC.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        lblRFC.setForeground(new java.awt.Color(255, 255, 255));
+        lblRFC.setText("RFC");
+        Background.add(lblRFC, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, -1, -1));
 
-        campoTextoEdad.setForeground(new java.awt.Color(51, 51, 51));
-        campoTextoEdad.setToolTipText("");
-        campoTextoEdad.setBorder(null);
-        Background.add(campoTextoEdad, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, 170, 20));
+        txtRFC.setForeground(new java.awt.Color(51, 51, 51));
+        txtRFC.setToolTipText("");
+        txtRFC.setBorder(null);
+        Background.add(txtRFC, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, 170, 20));
         Background.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 170, 10));
         Background.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 140, 170, 10));
         Background.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 200, 160, 10));
         Background.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 200, 160, 10));
 
-        lblNombres1.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 14)); // NOI18N
-        lblNombres1.setText("Marca");
-        Background.add(lblNombres1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, 180, 20));
+        lblMarca.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        lblMarca.setForeground(new java.awt.Color(255, 255, 255));
+        lblMarca.setText("Marca");
+        Background.add(lblMarca, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, 180, 20));
 
-        campoTextoNombres1.setForeground(new java.awt.Color(51, 51, 51));
-        campoTextoNombres1.setToolTipText("");
-        campoTextoNombres1.setBorder(null);
-        Background.add(campoTextoNombres1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 260, 170, 20));
+        txtMarca.setForeground(new java.awt.Color(51, 51, 51));
+        txtMarca.setToolTipText("");
+        txtMarca.setBorder(null);
+        Background.add(txtMarca, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 260, 170, 20));
         Background.add(jSeparator5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 220, 530, 10));
 
-        lblApellidoPaterno1.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 14)); // NOI18N
-        lblApellidoPaterno1.setText("Línea");
-        Background.add(lblApellidoPaterno1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 240, 180, 20));
+        lblLinea.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        lblLinea.setForeground(new java.awt.Color(255, 255, 255));
+        lblLinea.setText("Línea");
+        Background.add(lblLinea, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 240, 180, 20));
 
-        campoTextoApellidoPaterno1.setForeground(new java.awt.Color(51, 51, 51));
-        campoTextoApellidoPaterno1.setToolTipText("");
-        campoTextoApellidoPaterno1.setBorder(null);
-        Background.add(campoTextoApellidoPaterno1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 260, 160, 20));
+        txtLinea.setForeground(new java.awt.Color(51, 51, 51));
+        txtLinea.setToolTipText("");
+        txtLinea.setBorder(null);
+        Background.add(txtLinea, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 260, 160, 20));
         Background.add(jSeparator6, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 280, 160, 10));
 
-        lblApellidoMaterno1.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 14)); // NOI18N
-        lblApellidoMaterno1.setText("Modelo");
-        Background.add(lblApellidoMaterno1, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 240, 130, 20));
+        lblModelo.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        lblModelo.setForeground(new java.awt.Color(255, 255, 255));
+        lblModelo.setText("Modelo");
+        Background.add(lblModelo, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 240, 130, 20));
 
-        campoTextoApellidoMaterno1.setForeground(new java.awt.Color(51, 51, 51));
-        campoTextoApellidoMaterno1.setToolTipText("");
-        campoTextoApellidoMaterno1.setBorder(null);
-        Background.add(campoTextoApellidoMaterno1, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 260, 160, 20));
+        txtModelo.setForeground(new java.awt.Color(51, 51, 51));
+        txtModelo.setToolTipText("");
+        txtModelo.setBorder(null);
+        Background.add(txtModelo, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 260, 160, 20));
         Background.add(jSeparator7, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 280, 160, 10));
 
-        lblNombres2.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 14)); // NOI18N
-        lblNombres2.setText("Color");
-        Background.add(lblNombres2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, 180, 20));
+        lblColor.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        lblColor.setForeground(new java.awt.Color(255, 255, 255));
+        lblColor.setText("Color");
+        Background.add(lblColor, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, 180, 20));
 
-        campoTextoNombres2.setForeground(new java.awt.Color(51, 51, 51));
-        campoTextoNombres2.setToolTipText("");
-        campoTextoNombres2.setBorder(null);
-        Background.add(campoTextoNombres2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 310, 170, 20));
+        txtColor.setForeground(new java.awt.Color(51, 51, 51));
+        txtColor.setToolTipText("");
+        txtColor.setBorder(null);
+        Background.add(txtColor, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 310, 170, 20));
         Background.add(jSeparator8, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 330, 170, 10));
 
-        lblApellidoPaterno2.setFont(new java.awt.Font("Nirmala UI Semilight", 0, 14)); // NOI18N
-        lblApellidoPaterno2.setText("No. Serie");
-        Background.add(lblApellidoPaterno2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 290, 180, 20));
+        lblSerie.setFont(new java.awt.Font("Nirmala UI Semilight", 1, 14)); // NOI18N
+        lblSerie.setForeground(new java.awt.Color(255, 255, 255));
+        lblSerie.setText("No. Serie");
+        Background.add(lblSerie, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 290, 180, 20));
 
-        campoTextoApellidoPaterno2.setForeground(new java.awt.Color(51, 51, 51));
-        campoTextoApellidoPaterno2.setToolTipText("");
-        campoTextoApellidoPaterno2.setBorder(null);
-        Background.add(campoTextoApellidoPaterno2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 310, 160, 20));
+        txtSerie.setForeground(new java.awt.Color(51, 51, 51));
+        txtSerie.setToolTipText("");
+        txtSerie.setBorder(null);
+        Background.add(txtSerie, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 310, 160, 20));
         Background.add(jSeparator9, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 330, 160, 10));
         Background.add(jSeparator11, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 280, 170, 10));
 
@@ -226,25 +235,16 @@ public class RegistroAutomovil extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_btnRegistrarAutomovilActionPerformed
 
-    private void btnRegistrarAutomovil1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarAutomovil1ActionPerformed
+    private void btnCargarDatosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCargarDatosActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_btnRegistrarAutomovil1ActionPerformed
+    }//GEN-LAST:event_btnCargarDatosActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Background;
+    private javax.swing.JButton btnCargarDatos;
     private javax.swing.JButton btnRegistrarAutomovil;
-    private javax.swing.JButton btnRegistrarAutomovil1;
     private javax.swing.JButton btnRegresar;
-    private javax.swing.JTextField campoTextoApellidoMaterno;
-    private javax.swing.JTextField campoTextoApellidoMaterno1;
-    private javax.swing.JTextField campoTextoApellidoPaterno;
-    private javax.swing.JTextField campoTextoApellidoPaterno1;
-    private javax.swing.JTextField campoTextoApellidoPaterno2;
-    private javax.swing.JTextField campoTextoEdad;
-    private javax.swing.JTextField campoTextoNombres;
-    private javax.swing.JTextField campoTextoNombres1;
-    private javax.swing.JTextField campoTextoNombres2;
     private javax.swing.JButton jButton2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JSeparator jSeparator1;
@@ -258,15 +258,24 @@ public class RegistroAutomovil extends javax.swing.JFrame {
     private javax.swing.JSeparator jSeparator8;
     private javax.swing.JSeparator jSeparator9;
     private javax.swing.JLabel lblApellidoMaterno;
-    private javax.swing.JLabel lblApellidoMaterno1;
     private javax.swing.JLabel lblApellidoPaterno;
-    private javax.swing.JLabel lblApellidoPaterno1;
-    private javax.swing.JLabel lblApellidoPaterno2;
     private javax.swing.JLabel lblBienvenido;
-    private javax.swing.JLabel lblEdad;
+    private javax.swing.JLabel lblColor;
+    private javax.swing.JLabel lblLinea;
+    private javax.swing.JLabel lblMarca;
+    private javax.swing.JLabel lblModelo;
     private javax.swing.JLabel lblNombres;
-    private javax.swing.JLabel lblNombres1;
-    private javax.swing.JLabel lblNombres2;
+    private javax.swing.JLabel lblRFC;
+    private javax.swing.JLabel lblSerie;
+    private javax.swing.JTextField txtApellidoMaterno;
+    private javax.swing.JTextField txtApellidoPaterno;
+    private javax.swing.JTextField txtColor;
+    private javax.swing.JTextField txtLinea;
+    private javax.swing.JTextField txtMarca;
+    private javax.swing.JTextField txtModelo;
+    private javax.swing.JTextField txtNombres;
+    private javax.swing.JTextField txtRFC;
+    private javax.swing.JTextField txtSerie;
     // End of variables declaration//GEN-END:variables
 
 
