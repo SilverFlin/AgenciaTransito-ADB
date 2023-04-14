2866faed386b2bb7f2f4bcb7312a0cbb0dc0212d
DESKTOP-4RU6P74\march <misael.marchena233418@potros.itson.edu.mx>
2023-04-13 17:50:50 -0700

    codigo para la creación del reporte en pdf

--- 03_Implementation/agencia_transito/src/main/java/org/itson/utils/JasperByCollectionBeanData.java
+++ 03_Implementation/agencia_transito/src/main/java/org/itson/utils/JasperByCollectionBeanData.java
@@ -4,7 +4,25 @@
  */
 package org.itson.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 import org.itson.dominio.Tramite;
 
 /**
@@ -13,9 +31,29 @@ import org.itson.dominio.Tramite;
  */
 public class JasperByCollectionBeanData {
     
-    public JasperByCollectionBeanData(List<Tramite> listaTramites){
+    public JasperByCollectionBeanData(List<Tramite> listaTramites) throws JRException{
         String outputFile = "C:\\Users\\march\\OneDrive\\Escritorio\\AgenciaTransito-ADB\\03_Implementation\\agencia_transito\\src\\main\\resources\\pdfs" + "ReporteTramites.pdf";
         
         JRBeanCollectionDataSource itemsJRBean = new JRBeanCollectionDataSource(listaTramites);
+    
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("Parameter1", itemsJRBean);
+        
+        try {
+            InputStream input = new FileInputStream(new File("C:\\Users\\march\\OneDrive\\Escritorio\\AgenciaTransito-ADB\\03_Implementation\\agencia_transito\\src\\main\\resources\\pdfs\\ReporteTramites_A4.jrxml"));
+            
+            JasperDesign jasperDesign = JRXmlLoader.load(input);
+            
+            JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
+            
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
+            
+            JasperViewer.viewReport(jasperPrint);
+            
+            System.out.println("Archivo generado...");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JasperByCollectionBeanData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 }
